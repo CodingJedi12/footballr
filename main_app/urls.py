00bc782d58d9220bd3bbdbe0da56614fe8be6c39
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-10-24 15:19:33 -0400

    urls for update and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('myteams/', views.teams_index, name='index'),
     path('myteams/<int:team_id>/', views.teams_detail, name='detail'),
     path('myteams/create/', views.TeamCreate.as_view(), name='teams_create'),
+    path('myteams/<int:pk>/update/', views.TeamUpdate.as_view(), name='teams_update'),
+    path('myteams/<int:pk>/update/', views.TeamDelete.as_view(), name='teams_delete'),
 ]
