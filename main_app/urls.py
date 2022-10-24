b708566ed9da654853baea8ba5c2d1d0b3822157
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-10-24 12:02:09 -0400

    team detail template, url and view functional

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('myteams/', views.my_teams, name='index'),
+    path('myteams/', views.teams_index, name='index'),
+    path('myteams/<int:team_id>/', views.teams_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,12 +4,25 @@ from django.http import HttpResponse
 from .models import Team
 
 # Create your views here.
+# Renders the home page by connecting to the home.html template
 def home(request):
     return render(request, 'home.html')
 
+# Renders the about page by connecting to the about.html template
 def about(request):
     return render(request, 'about.html')
 
-def my_teams(request):
+# Renders the my teams page by connecting to the template
+def teams_index(request):
+    # Assigns a variable to the entire Team model object
     teams = Team.objects.all()
+    # Renders the type of request, routes to the teams template, and passes the variable we made into the template to be used
     return render(request, 'teams/index.html', {'teams': teams})
+
+# Renders the team detail (show) page by connecting to the template
+def teams_detail(request, team_id):
+    # Assings the team variable to a specific id in the Team model object that is passed as an argument
+    team = Team.objects.get(id=team_id)
+
+    # Renders the request, the detail template and passes the team variable through
+    return render(request, 'teams/detail.html', {'team': team})
