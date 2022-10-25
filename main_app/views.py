766254a257fde6ae004c4fea5b6b663fc1b526e5
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-10-25 16:58:58 -0400

    auth functional and team index conditional on user

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import RecordForm
 
@@ -20,13 +22,15 @@ def about(request):
     return render(request, 'about.html')
 
 # Renders the my teams page by connecting to the template
+@login_required
 def teams_index(request):
     # Assigns a variable to the entire Team model object
-    teams = Team.objects.all()
+    teams = Team.objects.filter(user=request.user)
     # Renders the type of request, routes to the teams template, and passes the variable we made into the template to be used
     return render(request, 'teams/index.html', {'teams': teams})
 
 # Renders the team detail (show) page by connecting to the template
+@login_required
 def teams_detail(request, team_id):
     # Creates a win and loss variable to be used
     wins = 0
@@ -57,6 +61,7 @@ def teams_detail(request, team_id):
         'players': players_not_on_team,
         })
 
+@login_required
 def add_game(request, team_id):
     # Sets form equal to the post request of a completed game form
     form = RecordForm(request.POST)
@@ -68,7 +73,7 @@ def add_game(request, team_id):
     return redirect('detail', team_id=team_id)
 
 # Creates a new team using the Team model and reroutes upon success
-class TeamCreate(CreateView):
+class TeamCreate(LoginRequiredMixin, CreateView):
     model = Team
     fields = ('name', 'league')
     success_url = '/myteams/'
@@ -81,49 +86,50 @@ class TeamCreate(CreateView):
         return super().form_valid(form)
 
 # Updates an existing team
-class TeamUpdate(UpdateView):
+class TeamUpdate(LoginRequiredMixin, UpdateView):
     model = Team
     fields = '__all__'
 
 # Deletes an existing team and reroutes upon success
-class TeamDelete(DeleteView):
+class TeamDelete(LoginRequiredMixin, DeleteView):
     model = Team
     success_url = '/myteams/'
 
 # Allows the player to be linked to the team
+@login_required
 def assoc_player(request, team_id, player_id):
     Team.objects.get(id=team_id).players.add(player_id)
     return redirect('detail', team_id=team_id)
 
 # Links model to create template
-class PlayerCreate(CreateView):
+class PlayerCreate(LoginRequiredMixin, CreateView):
     model = Player
     fields = ('name', 'position', 'nfl_team')
 
 # Links model to update template
-class PlayerUpdate(UpdateView):
+class PlayerUpdate(LoginRequiredMixin, UpdateView):
     model = Player
     fields = ('name', 'position', 'nfl_team')
 
 # Links model to delete template
-class PlayerDelete(DeleteView):
+class PlayerDelete(LoginRequiredMixin, DeleteView):
     model = Player
     success_url = '/players'
 
 # Links model with index template
-class PlayerList(ListView):
+class PlayerList(LoginRequiredMixin, ListView):
     model = Player
     template_name = 'players/index.html'
 
 # Links model with detail template
-class PlayerDetail(DetailView):
+class PlayerDetail(LoginRequiredMixin, DetailView):
     model = Player
     template_name = 'players/detail.html'
 
 # Signup view
 def signup(request):
     error_message = ''
-    if request.method == 'post':
+    if request.method == 'POST':
         # This is how we create a 'user' form object that allows us to use data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
